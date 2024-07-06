c29f6dad88f43009bbe380bf54433e687e738428
Natalija Toncic <natalijatoncic123@gmail.com>
2024-07-06 18:52:18 +0200

    title.tsx

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Title = () => {
-  return (
-    <div>Title</div>
-  )
+interface TitleProps {
+  text: string;
 }
 
-export default Title
+const Title: React.FC<TitleProps> = ({ text }) => {
+  return (
+    <div>
+      <div className='all-title-box'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-12'>
+              <h2>{text}</h2>
+              <ul className='breadcrumb'>
+                <li className='breadcrumb-item'>
+                  <Link to='/'>Home</Link>
+                </li>
+                <li className='breadcrumb-item active'>{text}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Title;
